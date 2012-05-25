9c085cfd1ffb596746fcd1ad1ecf34ba53dae12f
Sanil Singh Tomar <sanil.sun@gmail.com>
2012-05-25 09:31:26 +0530

    First Version - Working

---
+++ LyncDemo/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace LyncDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ReloadStatus = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AllowUserToResizeRows = false;
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView.Location = new System.Drawing.Point(0, 54);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(584, 408);
+            this.dataGridView.TabIndex = 2;
+            this.dataGridView.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellValueChanged);
+            this.dataGridView.RowValidated += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_RowValidated);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(221, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(203, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 60000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ReloadStatus
+            // 
+            this.ReloadStatus.Location = new System.Drawing.Point(505, 14);
+            this.ReloadStatus.Name = "ReloadStatus";
+            this.ReloadStatus.Size = new System.Drawing.Size(75, 23);
+            this.ReloadStatus.TabIndex = 3;
+            this.ReloadStatus.Text = "Refresh";
+            this.ReloadStatus.UseVisualStyleBackColor = true;
+            this.ReloadStatus.Click += new System.EventHandler(this.ReloadStatus_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 462);
+            this.Controls.Add(this.ReloadStatus);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(600, 500);
+            this.MinimumSize = new System.Drawing.Size(600, 250);
+            this.Name = "Form1";
+            this.Text = "Lync Contact Monitor";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button ReloadStatus;
+    }
+}
+
---
+++ LyncDemo/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System.Drawing;
+using System.IO;
+using System;
+using System.Windows.Forms;
+using Microsoft.Lync.Model;
+using System.Collections.Generic;
+
+namespace LyncDemo
+{
+    public partial class Form1 : Form
+    {
+        public System.Data.DataSet DS;
+        
+        string TableName = "LyncUsers", DataSetName= "Lync";
+        int offset = 50;
+        string FileName = Path.Combine(Application.StartupPath,"Lync.xml");
+        List<Contact> contacts;
+        LyncClient lyncClient;
+
+        public Form1()
+        {
+            InitializeComponent();
+            if (lyncClient == null)
+            {
+                lyncClient = LyncClient.GetClient();
+                lyncClient.StateChanged += new EventHandler<ClientStateChangedEventArgs>(lyncClient_StateChanged);
+            }
+            contacts = new List<Contact>();
+            InitDataSet();
+        }
+
+        void lyncClient_StateChanged(object sender, ClientStateChangedEventArgs e)
+        {
+            if (e.NewState != ClientState.SignedIn)
+            {
+                MessageBox.Show("Lync state changed: " + e.NewState.ToString() + "\r\n Waiting for signing in again");
+                System.Threading.Thread.Sleep(10000);
+            }
+        }
+
+        public void InitDataSet()
+        {
+            DS = new System.Data.DataSet(DataSetName);
+            if (File.Exists(FileName))
+            {
+                DS.ReadXml(FileName);
+                foreach (System.Data.DataRow row in DS.Tables[TableName].Rows)
+                {
+                    AddContact((string)row.ItemArray[4]);
+                }
+            }
+            else
+            {                
+                DS.Tables.Add(TableName);
+                DS.Tables[TableName].Columns.Add("User", typeof(string));
+                DS.Tables[TableName].Columns.Add("Status", typeof(string));
+                DS.Tables[TableName].Columns.Add("Change Time)", typeof(string));
+                DS.Tables[TableName].Columns.Add("State Time", typeof(string));
+                DS.Tables[TableName].Columns.Add("Email", typeof(string));
+                DS.Tables[TableName].PrimaryKey = new System.Data.DataColumn[] { DS.Tables[TableName].Columns[DS.Tables[TableName].Columns.IndexOf("User")] };
+            }
+            dataGridView.DataSource = DS;
+            dataGridView.DataMember = TableName;
+
+            timer1.Start();
+            RefreshStatus();
+        }
+        public void AddContact(string UserName)
+        {
+            if (lyncClient == null)
+            {
+                lyncClient = LyncClient.GetClient();
+            }
+            if (lyncClient.State == ClientState.SignedIn)
+            {
+                try
+                {
+                    Contact contact = lyncClient.ContactManager.GetContactByUri("sip:" + UserName);
+                    contacts.Add(contact);
+                    contact.ContactInformationChanged += new System.EventHandler<ContactInformationChangedEventArgs>(contact_ContactInformationChanged);
+                    Microsoft.Lync.Model.ContactAvailability availability = (Microsoft.Lync.Model.ContactAvailability)contact.GetContactInformation(ContactInformationType.Availability);
+
+                    string Name = (string)contact.GetContactInformation(ContactInformationType.DisplayName);
+                    for (int rowIndex = 0; rowIndex < dataGridView.Rows.Count; rowIndex++)
+                    {
+                        if ((string)dataGridView.Rows[rowIndex].Cells["User"].Value == Name)
+                        {
+                            dataGridView.Rows[rowIndex].Cells["Status"].Value = availability.ToString();
+                            break;
+                        }
+                    }
+                }
+                catch { }
+            }
+        }
+
+        public void RefreshStatus()
+        {
+            try
+            {
+                for (int rowIndex = 0; rowIndex < dataGridView.Rows.Count; rowIndex++)
+                {
+                    string email = (string)dataGridView.Rows[rowIndex].Cells["Email"].Value;
+                    Contact contact = lyncClient.ContactManager.GetContactByUri("sip:" + email);
+                    Microsoft.Lync.Model.ContactAvailability availability = (Microsoft.Lync.Model.ContactAvailability)contact.GetContactInformation(ContactInformationType.Availability);
+                    dataGridView.Rows[rowIndex].Cells["Status"].Value = availability.ToString();
+                    Color rowColor = GetColor(availability.ToString());
+
+                    DataGridViewCellCollection cells = dataGridView.Rows[rowIndex].Cells;
+                    foreach (DataGridViewCell cell in cells)
+                    {
+                        cell.Style.BackColor = rowColor;
+                    }
+                }
+            }
+            catch { } //Bad Programming
+        }
+
+        public void GetInfo(string UserName)
+        {
+            if (lyncClient == null)
+            {
+                lyncClient = LyncClient.GetClient();
+            }
+            if (lyncClient.State == ClientState.SignedIn)
+            {
+                try
+                {
+                    //Contact contact = lyncClient.ContactManager.GetContactByUri("sip:sourav.sengupta@accenture.com");
+                    Contact contact = lyncClient.ContactManager.GetContactByUri("sip:" + UserName);
+                    contacts.Add(contact);
+                    contact.ContactInformationChanged += new System.EventHandler<ContactInformationChangedEventArgs>(contact_ContactInformationChanged);
+                    Microsoft.Lync.Model.ContactAvailability availability = (Microsoft.Lync.Model.ContactAvailability)contact.GetContactInformation(ContactInformationType.Availability);
+                    string Name = (string)contact.GetContactInformation(ContactInformationType.DisplayName);
+                    System.Data.DataRow row = DS.Tables[TableName].Rows.Add(new string[] { Name, availability.ToString(), "0", "0", UserName });
+                }
+                catch
+                { }
+            }
+        }
+
+        void contact_ContactInformationChanged(object sender, ContactInformationChangedEventArgs e)
+        {
+            try
+            {
+                int i = e.ChangedContactInformation.IndexOf(ContactInformationType.Availability);
+                if (i >= 0)
+                {
+                    Contact contact = (Contact)sender;
+                    Microsoft.Lync.Model.ContactAvailability availability = (Microsoft.Lync.Model.ContactAvailability)contact.GetContactInformation(ContactInformationType.Availability);
+                    string Name = (string)contact.GetContactInformation(ContactInformationType.DisplayName);
+                    for (int rowIndex = 0; rowIndex < dataGridView.Rows.Count; rowIndex++)
+                    {
+                        if ((string)dataGridView.Rows[rowIndex].Cells["User"].Value == Name)
+                        {
+                            dataGridView.Rows[rowIndex].Cells["Status"].Value = availability.ToString();
+                            dataGridView.Rows[rowIndex].Cells["Change Time"].Value = DateTime.Now.ToString("hh:mm:ss tt dd-MMM");
+                            dataGridView.Rows[rowIndex].Cells["State Time"].Value = "0";
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (NotSignedInException ex)
+            {
+                if (lyncClient.State != ClientState.SignedIn)
+                {
+                    MessageBox.Show("Waiting for Lync to sign in\r\n"+ex.Message);
+                    System.Threading.Thread.Sleep(10000);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            if (textBox1.Text.Length == 0)
+            {
+                errorProvider1.SetError(textBox1, "Text box must not be empty");
+                errorProvider1.SetIconAlignment(textBox1, ErrorIconAlignment.MiddleLeft);
+                errorProvider1.SetIconPadding(textBox1, -20);
+                return;
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
+            GetInfo(textBox1.Text);
+            textBox1.Clear();
+        }
+
+        private void Form1_Resize(object sender, System.EventArgs e)
+        {
+            dataGridView.Height = this.ClientRectangle.Height - offset;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DS.WriteXml(FileName,System.Data.XmlWriteMode.WriteSchema);
+        }
+
+        private void dataGridView_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewCellCollection cells = dataGridView.Rows[e.RowIndex].Cells;
+            Color cellColor;
+            try
+            {
+                cellColor = GetColor((string)cells["Status"].Value);
+            }
+            catch
+            {
+                cellColor = Color.White;
+            }
+            foreach (DataGridViewCell cell in cells)
+            {
+                cell.Style.BackColor = cellColor;
+            }
+        }
+
+        public Color GetColor(string availability)
+        {
+            if (availability == "Away")
+            {
+                return Color.Yellow;
+            }
+            if (availability == "DoNotDisturb")
+            {
+                return Color.Red;
+            }
+            if (availability == "Busy")
+            {
+                return Color.Orange;
+            }
+            if (availability == "FreeIdle")
+            {
+                return Color.GreenYellow;
+            }
+            if (availability == "Free")
+            {
+                return Color.Green;
+            }
+            if (availability == "Offline")
+            {
+                return Color.LightGray;
+            }
+            return Color.White;
+        }
+
+        private void dataGridView_RowValidated(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewCellCollection cells = dataGridView.Rows[e.RowIndex].Cells;
+            Color cellColor = GetColor((string)cells["Status"].Value);
+            foreach (DataGridViewCell cell in cells)
+            {
+                cell.Style.BackColor = cellColor;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, System.EventArgs e)
+        {
+            errorProvider1.Clear();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int rowIndex = 0; rowIndex < dataGridView.Rows.Count; rowIndex++)
+            {
+                int time;
+                try
+                {
+                    time = int.Parse((string)dataGridView.Rows[rowIndex].Cells["State Time"].Value);
+                }
+                catch
+                {
+                    time = 0;
+                }
+                time++;
+                dataGridView.Rows[rowIndex].Cells["State Time"].Value = time.ToString();
+            }
+        }
+
+        private void ReloadStatus_Click(object sender, EventArgs e)
+        {
+            RefreshStatus();
+        }
+    }
+}
